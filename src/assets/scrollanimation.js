a974ac1c6268b65e42baaf78c07238a0727b0f88
Ararat01 <ararat12bars@gmail.com>
2021-12-27 21:45:49 +0400

    added scroll animation

---
+++ src/assets/scrollanimation.js
@@ -0,0 +1,28 @@
+window.addEventListener('scroll', animator)
+
+function offset(el) {
+    const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + screenLeft }
+}
+function animator() {
+    const animItems = document.querySelectorAll('.anim');
+    for (let i = 0; i < animItems.length; i++) {
+        const animItem = animItems[i]
+        const itemOffset = offset(animItem).top
+        const animStart = 4
+        if (animItem) {
+            const itemHeight = animItem.offsetHeight
+            let animItemPoint = window.innerHeight - itemHeight / animStart;
+
+            if (itemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((window.pageYOffset > itemOffset - animItemPoint) && window.pageYOffset < (itemOffset + itemHeight)) {
+                animItem.classList.add('is_active')
+            }
+        }
+    }
+}
